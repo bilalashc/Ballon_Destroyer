d6ab9e3bf940328e5a854d1a00a145603193386a
Bilal Choudhry <bilalchoudhry@gmail.com>
2023-04-02 19:53:11 +0300

    define constants

---
+++ src/scripts/constants.js
@@ -0,0 +1,39 @@
+export const DIFFICULTIES = {
+  EASY: 0.5,
+  MEDIUM: 0.9,
+  HARD: 1.4,
+};
+
+export const NUMBER_OF_BALLOONS = 52;
+export const BALLOON_RADIUS = 6;
+export const BALLOON_FILL_COLOR = 'yellow';
+export const BALLOON_TEXT_FONT = '0.6em Georgia';
+export const BALLOON_TEXT_FILL_COLOR = 'red';
+
+export const WORD_LETTERS = [
+  'A',
+  'B',
+  'c',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'Y',
+  'Z',
+];
