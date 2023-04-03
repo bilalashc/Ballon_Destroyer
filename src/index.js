77a2b52ef1f76ff992b422698c38e878d88bdc73
Bilal Choudhry <bilalchoudhry@gmail.com>
2023-04-03 06:59:53 +0300

    game control setup and intialization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import { DIFFICULTIES } from './scripts/constants';
+import { Game } from './scripts/game';
 import { initialezeGameControl } from './scripts/game-control';
 
 document.addEventListener('DOMContentLoaded', function () {
   initialezeGameControl();
+  const game = new Game(DIFFICULTIES.MEDIUM);
+  game.run()
 });
--- src/scripts/game-control.js
+++ src/scripts/game-control.js
@@ -1,33 +1,79 @@
-import background1 from '../images/1.jpg';
-import background2 from '../images/2.png';
-import background3 from '../images/3.jpg';
-
-function initialezeGameControl(canvasContext) {
-  const bg1 = document.getElementById('bg1');
+function initialezeGameControl() {
+  // const bg1 = document.getElementById('bg1');
   const bg2 = document.getElementById('bg2');
   const bg3 = document.getElementById('bg3');
 
-  bg1.addEventListener('click', () => {
-    setCanvasBackground(background1);
-  });
+  // bg1.addEventListener('click', () => {
+  //   setCanvasBackground('bg1');
+  // });
   bg2.addEventListener('click', () => {
-    setCanvasBackground(background2);
+    setCanvasBackground('bg2');
   });
   bg3.addEventListener('click', () => {
-    setCanvasBackground(background3);
+    setCanvasBackground('bg3');
   });
 
-  setCanvasBackground(background2);
+  setCanvasBackground('bg2');
+  // runingRect();
+}
+let y = 10;
+let x = 20;
+let direction = 'down';
+function runingRect() {
+  const [canvas, context] = getCanvasAndContext();
+  // context.restore()
+  // context.fillStyle = 'none'
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  // context.clearRect(x-6,y-6,12, 12)
+  context.beginPath();
+  context.fillStyle = 'yellow';
+  context.strokeStyle = 'black';
+  context.font = '0.6em Georgia';
+  // context.lineWidth = 5;
+  context.arc(x, y, 6, 0, 2 * Math.PI);
+  context.fill();
+  context.beginPath();
+  context.fillStyle = 'red';
+  context.fillText('A', x - 3, y + 3);
+  context.fill();
+
+  // context.clearRect(0, 0, canvas.width, canvas.height)
+  // context.clearRect(x + 20-6,y+20-6,12, 12)
+  context.beginPath();
+  context.fillStyle = 'yellow';
+  context.strokeStyle = 'black';
+  context.font = '0.6em Georgia';
+  // context.lineWidth = 5;
+  context.arc(x+20, y+20, 6, 0, 2 * Math.PI);
+  context.fill();
+  context.beginPath();
+  context.fillStyle = 'red';
+  context.fillText('B', x + 20- 3, y +20+ 3);
+  context.fill();
+  if (y <= 0) {
+    direction = 'down';
+  }
+
+  if (y >= 145) {
+    direction = 'up';
+    console.log('nwo it shoud go up')
+  }
+
+  direction === 'down' ? y += 0.2 : y -= 0.2;
+  // console.log('x,y', x, y, canvas.height, canvas.width)
+  // ctx.fillRect(50, x, 20, 20);
+  requestAnimationFrame(runingRect);
 }
 
 function setCanvasBackground(image) {
+  const canvasContainer = document.getElementById('canvas_container');
+  canvasContainer.setAttribute('class', `container-right ${image}`);
+}
+
+function getCanvasAndContext() {
   const canvas = document.getElementById('game_canvas');
   const context = canvas.getContext('2d');
-  const img = new Image();
-  img.src = image;
-  img.onload = () => {
-    context.drawImage(img, 0, 0, canvas.width, canvas.height);
-  };
+  return [canvas, context];
 }
 
 export { initialezeGameControl };
