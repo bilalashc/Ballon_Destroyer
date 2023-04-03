a1d45fbde46a5abe358b6b062e1f6951696128a6
Bilal Choudhry <bilalchoudhry@gmail.com>
2023-04-03 19:37:33 +0300

    remove unused code

--- src/scripts/game-control.js
+++ src/scripts/game-control.js
@@ -16,64 +16,10 @@ function initialezeGameControl() {
   setCanvasBackground('bg2');
   // runingRect();
 }
-let y = 10;
-let x = 20;
-let direction = 'down';
-function runingRect() {
-  const [canvas, context] = getCanvasAndContext();
-  // context.restore()
-  // context.fillStyle = 'none'
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  // context.clearRect(x-6,y-6,12, 12)
-  context.beginPath();
-  context.fillStyle = 'yellow';
-  context.strokeStyle = 'black';
-  context.font = '0.6em Georgia';
-  // context.lineWidth = 5;
-  context.arc(x, y, 6, 0, 2 * Math.PI);
-  context.fill();
-  context.beginPath();
-  context.fillStyle = 'red';
-  context.fillText('A', x - 3, y + 3);
-  context.fill();
-
-  // context.clearRect(0, 0, canvas.width, canvas.height)
-  // context.clearRect(x + 20-6,y+20-6,12, 12)
-  context.beginPath();
-  context.fillStyle = 'yellow';
-  context.strokeStyle = 'black';
-  context.font = '0.6em Georgia';
-  // context.lineWidth = 5;
-  context.arc(x+20, y+20, 6, 0, 2 * Math.PI);
-  context.fill();
-  context.beginPath();
-  context.fillStyle = 'red';
-  context.fillText('B', x + 20- 3, y +20+ 3);
-  context.fill();
-  if (y <= 0) {
-    direction = 'down';
-  }
-
-  if (y >= 145) {
-    direction = 'up';
-    console.log('nwo it shoud go up')
-  }
-
-  direction === 'down' ? y += 0.2 : y -= 0.2;
-  // console.log('x,y', x, y, canvas.height, canvas.width)
-  // ctx.fillRect(50, x, 20, 20);
-  requestAnimationFrame(runingRect);
-}
 
 function setCanvasBackground(image) {
   const canvasContainer = document.getElementById('canvas_container');
   canvasContainer.setAttribute('class', `container-right ${image}`);
-}
-
-function getCanvasAndContext() {
-  const canvas = document.getElementById('game_canvas');
-  const context = canvas.getContext('2d');
-  return [canvas, context];
 }
 
 export { initialezeGameControl };
